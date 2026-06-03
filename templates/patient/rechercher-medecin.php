17a0b04f5d740ccdd4f5c9e3a045873a1bdb7633
SALWA507 <salwaamzane30@gmail.com>
2026-06-03 12:06:29 +0100

    travaille sur la partie de recherche doctor

--- templates/patient/rechercher-medecin.php
+++ templates/patient/rechercher-medecin.php
@@ -1,1 +1,57 @@
-<php ?> teste </php>
+<?php
+
+require '../config/database.php';
+
+$search = $_GET['search'] ?? '';
+
+$sql = "
+SELECT
+doctor.id_doctor,
+user.nom,
+specialite.nom AS specialite
+
+FROM doctor
+
+JOIN user
+ON doctor.user_id = user.id
+
+JOIN specialite
+ON doctor.specialite_id = specialite.id
+
+WHERE user.nom LIKE ?
+OR specialite.nom LIKE ?
+";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+"%$search%",
+"%$search%"
+]);
+
+$doctors = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<form method="GET">
+    <input type="text" name="search" placeholder="Nom ou spécialité">
+    <button>Rechercher</button>
+</form>
+
+<hr>
+
+<?php foreach($doctors as $doctor): ?>
+
+<h3><?= $doctor['nom']; ?></h3>
+
+<p>
+Spécialité :
+<?= $doctor['specialite']; ?>
+</p>
+
+<a href="reservation.php?id=<?= $doctor['id_doctor']; ?>">
+Réserver
+</a>
+
+<hr>
+
+<?php endforeach; ?>
