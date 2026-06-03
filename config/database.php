3ec5f2abd391b06bad7e4ccf1f177579e697cb00
ennafiaasma-art <ennafiaasma@gmail.com>
2026-06-03 16:19:54 +0100

    la connection avec la base de donnees

--- config/database.php
+++ config/database.php
@@ -1,1 +1,38 @@
-<php ?> teste </php>
+<?php
+require_once __DIR__ . "/Env.php";
+
+Env::load(__DIR__ . "/../../.env");
+class Database {
+
+    private static ?PDO $instance = null;
+
+    private function __construct() {}
+
+    public static function getInstance(): PDO {
+
+        if (self::$instance === null) {
+
+            try {
+
+                self::$instance = new PDO(
+                    "mysql:host=" . $_ENV['DB_HOST'] .
+                    ";dbname=" . $_ENV['DB_NAME'],
+                    $_ENV['DB_USER'],
+                    $_ENV['DB_PASS']
+                );
+
+                self::$instance->setAttribute(
+                    PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION
+                );
+
+            } catch (PDOException $e) {
+
+                die("Connexion échouée : " . $e->getMessage());
+            }
+        }
+
+        return self::$instance;
+    }
+}
+?>
