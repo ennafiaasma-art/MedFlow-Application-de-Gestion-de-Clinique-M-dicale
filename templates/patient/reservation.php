a74cc1a600a5879493223d0a7051be18de33a563
SALWA507 <salwaamzane30@gmail.com>
2026-06-03 12:52:50 +0100

    Créer rendez-vous

--- templates/patient/reservation.php
+++ templates/patient/reservation.php
@@ -1,1 +1,36 @@
-<php ?> teste </php>
+<?php
+
+require '../config/database.php';
+
+$doctorId = $_GET['id'];
+
+if(isset($_POST['reserver'])){
+
+    $patientId = 1;
+
+    $disponibiliteId = $_POST['disponibilite'];
+
+    $sql = "
+    INSERT INTO rendezvous
+    (
+        statut,
+        patient_id,
+        disponibilite_id
+    )
+    VALUES
+    (
+        'en_attente',
+        ?,
+        ?
+    )
+    ";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        $patientId,
+        $disponibiliteId
+    ]);
+
+    echo "Rendez-vous réservé";
+}
