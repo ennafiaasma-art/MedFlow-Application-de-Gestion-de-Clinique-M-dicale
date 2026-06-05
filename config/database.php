88de316f81fc6dd747daa74beeed54d3c29eee10
SALWA507 <salwaamzane30@gmail.com>
2026-06-05 17:36:33 +0100

    Modification database

--- config/database.php
+++ config/database.php
@@ -1,18 +1,45 @@
 <?php
 
-$host = "localhost";
-$dbname = "medflow";
-$user = "root";
-$password = "";
+require_once __DIR__ . "/Env.php";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password );
+Env::load(__DIR__ . "/src/.env");
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connexion réussie !";
+class Database
+{
+    private static ?PDO $instance = null;
 
-} catch(PDOException $e) 
-  {
-    die("Erreur : " . $e->getMessage());
-  }
-?>
+    private function __construct() {}
+
+    public static function getInstance(): PDO
+    {
+        if (self::$instance === null) {
+
+            try {
+
+                self::$instance = new PDO(
+                    "mysql:host=" . $_ENV['DB_HOST'] .
+                    ";dbname=" . $_ENV['DB_NAME'] .
+                    ";charset=utf8mb4",
+                    $_ENV['DB_USER'],
+                    $_ENV['DB_PASS']
+                );
+
+                self::$instance->setAttribute(
+                    PDO::ATTR_ERRMODE,
+                    PDO::ERRMODE_EXCEPTION
+                );
+
+                self::$instance->setAttribute(
+                    PDO::ATTR_DEFAULT_FETCH_MODE,
+                    PDO::FETCH_ASSOC
+                );
+
+            } catch (PDOException $e) {
+
+                die("Connexion échouée : " . $e->getMessage());
+            }
+        }
+
+        return self::$instance;
+    }
+}
